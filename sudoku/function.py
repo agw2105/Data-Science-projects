dfb880a86c5faf782228cba357e9532c6d98dde8
agw2105 <48716562+agw2105@users.noreply.github.com>
2021-03-15 14:34:12 -0400

    Add files via upload

---
+++ sudoku/function.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 18:05:39 2020
+
+@author: alysonweidmann
+"""
+
+
+from sudoku.utils import *
+
+
+grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+
+def grid_values(grid):
+    assert len(grid) == 81
+    values = []
+    all_digits = '123456789'
+    for c in grid:
+        if c == '.':
+            values.append(all_digits)
+        elif c in all_digits:
+            values.append(c)
+    return dict(zip(boxes, values))
+
+def eliminate(values):
+      
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def only_choice(values):
+    for unit in unit_list:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+        
+    return values
+
+def reduce_puzzle(values):
+    """Constraint propagation"""
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for value in values[s]:
+        new = values.copy()
+        new[s] = value
+        attempt = search(new)
+        
+        if attempt:
+            return attempt
+                
+    # If you're stuck, see the solution.py tab!
---
+++ sudoku/utils.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 09:40:10 2020
+
+@author: alysonweidmann
+"""
+from collections import defaultdict
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(a,b):
+    return [s+t for s in a for t in b]
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+unitlist = row_units+column_units+square_units
+
+diag1 = [r+c for r,c in zip(rows, cols)]
+diag2 = [r+c for r,c in zip(rows, cols[::-1])]
+diagonal_units = [diag1, diag2]
+
+# TODO: Update the unit list to add the new diagonal units
+unitlist.extend(diagonal_units)
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Input: The sudoku in dictionary form
+    Output: None
+    """
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+
+history = {}  # history must be declared here so that it exists in the assign_values scope
+
+
+def extract_units(unitlist, boxes):
+    """Initialize a mapping from box names to the units that the boxes belong to
+
+    Parameters
+    ----------
+    unitlist(list)
+        a list containing "units" (rows, columns, diagonals, etc.) of boxes
+
+    boxes(list)
+        a list of strings identifying each box on a sudoku board (e.g., "A1", "C7", etc.)
+
+    Returns
+    -------
+    dict
+        a dictionary with a key for each box (string) whose value is a list
+        containing the units that the box belongs to (i.e., the "member units")
+    """
+    # the value for keys that aren't in the dictionary are initialized as an empty list
+    units = defaultdict(list)
+    for current_box in boxes:
+        for unit in unitlist:
+            if current_box in unit:
+                # defaultdict avoids this raising a KeyError when new keys are added
+                units[current_box].append(unit)
+    return units
+
+
+def extract_peers(units, boxes):
+    """Initialize a mapping from box names to a list of peer boxes (i.e., a flat list
+    of boxes that are in a unit together with the key box)
+
+    Parameters
+    ----------
+    units(dict)
+        a dictionary with a key for each box (string) whose value is a list
+        containing the units that the box belongs to (i.e., the "member units")
+
+    boxes(list)
+        a list of strings identifying each box on a sudoku board (e.g., "A1", "C7", etc.)
+
+    Returns
+    -------
+    dict
+        a dictionary with a key for each box (string) whose value is a set
+        containing all boxes that are peers of the key box (boxes that are in a unit
+        together with the key box)
+    """
+    # the value for keys that aren't in the dictionary are initialized as an empty list
+    peers = defaultdict(set)  # set avoids duplicates
+    for key_box in boxes:
+        for unit in units[key_box]:
+            for peer_box in unit:
+                if peer_box != key_box:
+                    # defaultdict avoids this raising a KeyError when new keys are added
+                    peers[key_box].add(peer_box)
+    return peers
+
+
+def assign_value(values, box, value):
+    """You must use this function to update your values dictionary if you want to
+    try using the provided visualization tool. This function records each assignment
+    (in order) for later reconstruction.
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict
+        The values dictionary with the naked twins eliminated from peers
+    """
+    # Don't waste memory appending actions that don't actually change any values
+    if values[box] == value:
+        return values
+
+    prev = values2grid(values)
+    values[box] = value
+    if len(value) == 1:
+        history[values2grid(values)] = (prev, (box, value))
+    return values
+
+def values2grid(values):
+    """Convert the dictionary board representation to as string
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    """
+    res = []
+    for r in rows:
+        for c in cols:
+            v = values[r + c]
+            res.append(v if len(v) == 1 else '.')
+    return ''.join(res)
+
+
+def grid2values(grid):
+    """Convert grid into a dict of {square: char} with '123456789' for empties.
+
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    
+    Returns
+    -------
+        A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value,
+            then the value will be '123456789'.
+    """
+    sudoku_grid = {}
+    for val, key in zip(grid, boxes):
+        if val == '.':
+            sudoku_grid[key] = '123456789'
+        else:
+            sudoku_grid[key] = val
+    return sudoku_grid
+
+def reconstruct(values, history):
+    """Returns the solution as a sequence of value assignments 
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    history(dict)
+        a dictionary of the form {key: (key, (box, value))} encoding a linked
+        list where each element points to the parent and identifies the value
+        assignment that connects from the parent to the current state
+
+    Returns
+    -------
+    list
+        a list of (box, value) assignments that can be applied in order to the
+        starting Sudoku puzzle to reach the solution
+    """
+    path = []
+    prev = values2grid(values)
+    while prev in history:
+        prev, step = history[prev]
+        path.append(step)
+    return path[::-1]
+
